7e28f4e810d6dcb4de66def97819a15eb96b2ec6
daviu <daviu@DESKTOP-0BEA9Q2>
2023-11-23 19:42:34 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace EstruturaRepetitivaEnquanto
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int x, soma;
+            soma = 0;
+
+            x = int.Parse(Console.ReadLine());
+
+            //recomanda - se quando não se sabe ao certo quantas repetições haverá.
+
+            while (x != 0) //enquanto
+            {
+                soma = soma + x;
+
+                x = int.Parse(Console.ReadLine());
+
+
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+
+        }
+    }
+}
